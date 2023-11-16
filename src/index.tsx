bbd89396c057ac3b0b03e1b1e7699693b01d08ef
Ivan77web <ivanshestopalov39@gmail.com>
2023-11-16 15:01:36 +0300

    add react router + code splitting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 
@@ -11,5 +12,7 @@ if (!container) {
 const root = createRoot(container);
 
 root.render(
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
 );
